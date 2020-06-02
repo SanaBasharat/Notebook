452710d0dfc4f3d9e8ad4060734ec43597270f4b
SanaBasharat <sana_basharat98@hotmail.com>
2020-06-02 17:38:01 +0500

    mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var sql = require('mssql');
 const poolPromise = require('../db.js');
-//var Engine = require('./engine');
 var fs = require('fs');
 var mysql = require('mysql');
-//var Index = new Engine.Index();
+var MongoClient = require ("mongodb").MongoClient;
+var connection = 'mongodb://localhost:27017/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,56 +18,10 @@ router.post('/signupverify', async function(req, res, next) {
   var password = req.body.password;
   var verified = false;
   var uname, em, pass, found;
-  //try{
-    var sql = require("mssql");
 
-    // config for your database
-    var config = {
-        user: 'sana',
-        password: '12345678',
-        server: 'DESKTOP-PFBRNRR', 
-        database: 'notebookdb',
-        options:{
-            "enableArithAbort": true
-        },
-    };
+  
 
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from users', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
 });
-
-    async () => {
-      try {
-          // make sure that any items are correctly URL encoded in the connection string
-          await sql.connect('mssql://sa:12345678@localhost/notebookdb')
-          const result = await mysql.query`select * from users`
-          console.log(result)
-      } catch (err) {
-          // ... error checks
-      }
-  }
-  // }
-  // catch (err) {
-  //   next(err);
-  //   return;;
-  // }
-//});
 
 // router.post('/notebook', function(req, res, next) {
 //   console.log("111111111")
