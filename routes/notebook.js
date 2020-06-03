c463570981e91eb10895935fd65f5776fc395664
SanaBasharat <sana_basharat98@hotmail.com>
2020-06-03 15:01:37 +0500

    json reading done

--- routes/notebook.js
+++ routes/notebook.js
@@ -6,15 +6,35 @@ var session = require('express-session');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("i am at notebook");
-    var filePath = "./views/notebook.html";
+  //read Filenames from DB
+  var obj = {
+    username: "SanaB",
+    files: [
+      {filename: 'My Word File', image: '/images/doc_placeholder.png'},
+      {filename: 'SanaPPTX', image: '/images/doc_placeholder.png'},
+      {filename: 'Hello World', image: '/images/doc_placeholder.png'},
+      {filename: 'My File', image: '/images/doc_placeholder.png'},
+      {filename: 'Hello Kitty', image: '/images/doc_placeholder.png'}
+    ]
+  }
+  var myfiles = "";
+  var myusername = "";
+  obj.files.forEach(element => {
+    myfiles += "<div class='card'><img src='"+element.image+"' class='macimage'><p>"+element.filename+"</p></div>";
+  });
+  //var a = JSON.parse(obj);
+  myusername = obj.username;
+  console.log(obj.username);
+  var filePath = "./views/notebook.html";
     fs.readFile(filePath, (err, contents) => {
         if (err) {
             next(err);
-            return;;
+            return;
         }
-        HTMLrender = contents.toString().replace(/<!--Email-->/g, req.session.username);
+        HTMLrender = contents.toString().replace(/<!--username-->/g, myusername);
+        HTMLrender = HTMLrender.replace(/<!--files-->/g, myfiles);
         res.send(HTMLrender);
-        console.log("done");
+        //res.end();
     });
 });
 
